d4ee314a4b776bb2e42a7414f0d6a2769b60c853
Auerorian <107329445+Auerorian@users.noreply.github.com>
2023-12-26 21:30:17 -0700

    bruh again with the sooo many commits

--- background/jctl.py
+++ background/jctl.py
@@ -4,18 +4,19 @@ import re
 import signal
 import tempfile
 import time
+from sys import exit
 
-
+#make a way to input the s/n in the app.py and import or something into the "SERIAL_NUM" here and in other python scripts
 SINCE = "today"
 SERIAL_NUM = ""
 LOCAL_USER = os.getenv('SUDO_USER') or os.getenv('USER')
-PATTERNS = r"bin/PATTERNS"
-IGNORE_PATTERNS = r"bin/IGNORE_PATTERNS"
+PATTERNS = r"../bin/PATTERNS"
+IGNORE_PATTERNS = r"../bin/IGNORE_PATTERNS"
 
-
-# Function to handle cleanup
 def cleanup(signum, frame):
-    print("Cleaning up temporary files...")
+    print(" Cleaning up temporary files...")
+    temp_output.close()
+    temp_journal.close()
     os.remove(temp_output.name)
     os.remove(temp_journal.name)
     log_path = f"/home/{LOCAL_USER}/Desktop/journalctl{SERIAL_NUM}.log"
@@ -23,49 +24,30 @@ def cleanup(signum, frame):
         os.remove(log_path)
     exit(0)
 
-
-# Set signal handlers
 signal.signal(signal.SIGINT, cleanup)
 signal.signal(signal.SIGTERM, cleanup)
 
-
-# Check if the log file exists and remove it
 log_file_path = f"/home/{LOCAL_USER}/Desktop/journalctl{SERIAL_NUM}.log"
 if os.path.exists(log_file_path):
     os.remove(log_file_path)
 
-
-# Temporary files to store new lines.
 temp_output = tempfile.NamedTemporaryFile(delete=False)
 temp_journal = tempfile.NamedTemporaryFile(delete=False)
 
-
-# Function to handle file writing
 def write_to_log_file(data):
     with open(log_file_path, "a") as log_file:
         log_file.write(data)
 
-
-# Main loop
 while True:
-    # Run journalctl command and capture stdout
     journal_process = subprocess.Popen(["sudo", "journalctl", f"--since={SINCE}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-
-    # Get stdout and stderr
     journal_stdout, journal_stderr = journal_process.communicate()
 
-
-    # Check for errors in stderr
     if journal_stderr:
         print(f"Error running journalctl: {journal_stderr.decode()}")
 
-
-    # Process stdout
     journal_content = journal_stdout.decode()
 
-
-    # Find and write matching lines to temp_output
     pattern_matches = re.findall(PATTERNS, journal_content, flags=re.MULTILINE)
     ignore_pattern_matches = re.findall(IGNORE_PATTERNS, journal_content, flags=re.MULTILINE)
 
@@ -76,8 +58,6 @@ while True:
 
     SINCE = time.strftime('%Y-%m-%d %H:%M:%S')
 
-
-    # Process new lines
     with open(temp_output.name, "r") as output_file:
         for line in output_file:
             timestamp = line.split()[0] + " " + line.split()[1] + " " + line.split()[2]
@@ -98,8 +78,6 @@ while True:
             else:
                 block = line
 
-
-            # Write the block to the log file
             write_to_log_file(block)
 
 
