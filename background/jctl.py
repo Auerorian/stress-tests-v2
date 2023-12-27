2c75aa1d3993e891c92aca02070f6cd11b9be063
Auerorian <107329445+Auerorian@users.noreply.github.com>
2023-12-26 19:13:45 -0700

    Just testing the new branch

--- background/jctl.py
+++ background/jctl.py
@@ -1,5 +1,16 @@
+import os
 import subprocess
-from 
+import re
+import signal
+import tempfile
+import time
+
+SINCE = "today"
+SERIAL_NUM = ""
+LOCAL_USER = "your_local_user"  # Replace with the actual local user
+PATTERNS = r"your_patterns_regex"  # Replace with the actual patterns regex
+IGNORE_PATTERNS = r"your_ignore_patterns_regex"  # Replace with the actual ignore patterns regex
+
 
 def watch_journalctl():
     process = subprocess.Popen(['sudo', 'journalctl', '-f'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
