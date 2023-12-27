8901ad78c37542e7565f3a82c208e6905b67ed6b
Auerorian <107329445+Auerorian@users.noreply.github.com>
2023-12-26 21:01:05 -0700

    forced lf usage by git. also testing this on the laptoppppp :)

--- background/jctl.py
+++ background/jctl.py
@@ -1,4 +1,3 @@
-
 import os
 import subprocess
 import re
@@ -6,13 +5,16 @@ import signal
 import tempfile
 import time
 
+
 SINCE = "today"
 SERIAL_NUM = ""
 LOCAL_USER = "your_local_user"  # Replace with the actual local user
 PATTERNS = r"your_patterns_regex"  # Replace with the actual patterns regex
 IGNORE_PATTERNS = r"your_ignore_patterns_regex"  # Replace with the actual ignore patterns regex
 
-def cleanup():
+
+# Function to handle cleanup
+def cleanup(signum, frame):
     print("Cleaning up temporary files...")
     os.remove(temp_output.name)
     os.remove(temp_journal.name)
@@ -21,37 +23,65 @@ def cleanup():
         os.remove(log_path)
     exit(0)
 
+
+# Set signal handlers
 signal.signal(signal.SIGINT, cleanup)
 signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGTERM, cleanup)
 
-if os.path.exists(f"/home/{LOCAL_USER}/Desktop/journalctl{SERIAL_NUM}.log"):
-    os.remove(f"/home/{LOCAL_USER}/Desktop/journalctl{SERIAL_NUM}.log")
+
+# Check if the log file exists and remove it
+log_file_path = f"/home/{LOCAL_USER}/Desktop/journalctl{SERIAL_NUM}.log"
+if os.path.exists(log_file_path):
+    os.remove(log_file_path)
+
 
 # Temporary files to store new lines.
 temp_output = tempfile.NamedTemporaryFile(delete=False)
 temp_journal = tempfile.NamedTemporaryFile(delete=False)
 
+
+# Function to handle file writing
+def write_to_log_file(data):
+    with open(log_file_path, "a") as log_file:
+        log_file.write(data)
+
+
+# Main loop
 while True:
-    # Fetch the entire journal since the given timestamp.
-    subprocess.run(["sudo", "journalctl", f"--since={SINCE}"], stdout=temp_journal)
+    # Run journalctl command and capture stdout
+    journal_process = subprocess.Popen(["sudo", "journalctl", f"--since={SINCE}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    with open(temp_journal.name, "r") as journal_file:
-        journal_content = journal_file.read()
 
+    # Get stdout and stderr
+    journal_stdout, journal_stderr = journal_process.communicate()
+
+
+    # Check for errors in stderr
+    if journal_stderr:
+        print(f"Error running journalctl: {journal_stderr.decode()}")
+
+
+    # Process stdout
+    journal_content = journal_stdout.decode()
+
+
+    # Find and write matching lines to temp_output
     pattern_matches = re.findall(PATTERNS, journal_content, flags=re.MULTILINE)
     ignore_pattern_matches = re.findall(IGNORE_PATTERNS, journal_content, flags=re.MULTILINE)
+
 
     with open(temp_output.name, "w") as output_file:
         output_file.writelines(line for line in pattern_matches if line not in ignore_pattern_matches)
 
-    # Update SINCE to now, so the next iteration will pick up logs from this moment onward.
+
     SINCE = time.strftime('%Y-%m-%d %H:%M:%S')
 
-    # Process the new lines.
+
+    # Process new lines
     with open(temp_output.name, "r") as output_file:
         for line in output_file:
             timestamp = line.split()[0] + " " + line.split()[1] + " " + line.split()[2]
+
 
             if "[ cut here ]" in line:
                 block_start = f"{timestamp}.*[ cut here ]"
@@ -68,7 +98,9 @@ while True:
             else:
                 block = line
 
-            with open(f"/home/{LOCAL_USER}/Desktop/journalctl{SERIAL_NUM}.log", "a") as log_file:
-                log_file.write(block)
+
+            # Write the block to the log file
+            write_to_log_file(block)
+
 
     time.sleep(5)
